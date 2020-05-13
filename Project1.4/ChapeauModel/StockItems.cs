897fecf25291e279342f5bba0f7fd0157daff33a
Abhi6921 <54932316+Abhi6921@users.noreply.github.com>
2020-05-13 12:18:44 +0200

    I made the classes that I think i require for my part.

---
+++ Project1.4/ChapeauModel/StockItems.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChapeauModel
+{
+    public class StockItems
+    {
+        private int StockId;
+        private int cateagoryId;
+        private double totalprice; // total price for the stock ordered
+        private int amount; // checks the amount of stock ordered 
+        private string stockname;
+        private DateTime timeofarrival;
+
+        public StockItems(int StockId, int cateagoryId, double totalprice, int amount, string stockname, DateTime timeofarrival)
+        {
+            this.StockId = StockId;
+            this.cateagoryId = cateagoryId;
+            this.totalprice = totalprice;
+            this.amount = amount;
+            this.stockname = stockname;
+            this.timeofarrival = timeofarrival;
+        }
+                                    
+    }
+}
---
+++ Project1.4/Project1.4/ChapeauModel/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChapeauModel
+{
+    class Employee
+    {
+        private int EmployeeId;
+        private string FirstName;
+        private string LastName;
+        private string emailaddress;
+        private int phonenumber;
+
+        public Employee(int EmployeeId,string firstname,string lastname,string emailaddress,int phonenumber)
+        {
+            this.EmployeeId = EmployeeId;
+            this.FirstName = firstname;
+            this.LastName = lastname;
+            this.emailaddress = emailaddress;
+            this.phonenumber = phonenumber;
+        }
+    }
+}
