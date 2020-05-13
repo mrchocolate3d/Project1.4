74249a4132001cab4b9b8487a557a5155803364e
sharelyn_jones <sajones.21@hotmail.com>
2020-05-13 13:28:56 +0200

    Update Order.cs

--- Project1.4/ChapeauModel/Order.cs
+++ Project1.4/ChapeauModel/Order.cs
@@ -9,7 +9,34 @@ namespace ChapeauModel
     public class Order
     {
         public int OrderID;
+        public int TableNumber;
         public int EmployeeID;
         public string EmployeeName;
+        public int OrderDate;
+        public string Item;
+        public int Amount;
+        public string OrderStatus;
+
+        public void CreateOrder()
+        {
+
+        }
+
+        public void CreateItem()
+        {
+
+        }
+
+        public void AddItem()
+        {
+
+        }
+
+        public void DeleteItem()
+        {
+
+        }
+
+
     }
 }
