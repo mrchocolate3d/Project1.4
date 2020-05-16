aa9e2a18106fa740e5b5a3d55a3647921b75aae0
Seyifunmi Gandonu <655453@student.inholland.nl>
2020-05-16 20:40:49 +0200

    Login page Created

--- Project1.4/Main/LoginPage.Designer.cs
+++ Project1.4/Main/LoginPage.Designer.cs
@@ -28,44 +28,195 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.roundButton1 = new ChapeauUI.RoundButton();
-            this.roundButton2 = new ChapeauUI.RoundButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_0 = new ChapeauUI.RoundButton();
+            this.btn_9 = new ChapeauUI.RoundButton();
+            this.btn_8 = new ChapeauUI.RoundButton();
+            this.btn_7 = new ChapeauUI.RoundButton();
+            this.btn_6 = new ChapeauUI.RoundButton();
+            this.btn_5 = new ChapeauUI.RoundButton();
+            this.btn_4 = new ChapeauUI.RoundButton();
+            this.btn_3 = new ChapeauUI.RoundButton();
+            this.btn_2 = new ChapeauUI.RoundButton();
+            this.btn_1 = new ChapeauUI.RoundButton();
+            this.btn_delete = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // roundButton1
+            // textBox1
             // 
-            this.roundButton1.Location = new System.Drawing.Point(276, 110);
-            this.roundButton1.Name = "roundButton1";
-            this.roundButton1.Size = new System.Drawing.Size(75, 23);
-            this.roundButton1.TabIndex = 0;
-            this.roundButton1.Text = "roundButton1";
-            this.roundButton1.UseVisualStyleBackColor = true;
+            this.textBox1.Location = new System.Drawing.Point(139, 138);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 10;
             // 
-            // roundButton2
+            // btn_0
             // 
-            this.roundButton2.Location = new System.Drawing.Point(543, 230);
-            this.roundButton2.Name = "roundButton2";
-            this.roundButton2.Size = new System.Drawing.Size(75, 23);
-            this.roundButton2.TabIndex = 1;
-            this.roundButton2.Text = "roundButton2";
-            this.roundButton2.UseVisualStyleBackColor = true;
+            this.btn_0.FlatAppearance.BorderSize = 0;
+            this.btn_0.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_0.Location = new System.Drawing.Point(153, 425);
+            this.btn_0.Name = "btn_0";
+            this.btn_0.Size = new System.Drawing.Size(54, 51);
+            this.btn_0.TabIndex = 9;
+            this.btn_0.Text = "0";
+            this.btn_0.UseVisualStyleBackColor = true;
+            this.btn_0.Click += new System.EventHandler(this.btn_0_Click);
+            // 
+            // btn_9
+            // 
+            this.btn_9.FlatAppearance.BorderSize = 0;
+            this.btn_9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_9.Location = new System.Drawing.Point(237, 368);
+            this.btn_9.Name = "btn_9";
+            this.btn_9.Size = new System.Drawing.Size(54, 51);
+            this.btn_9.TabIndex = 8;
+            this.btn_9.Text = "9";
+            this.btn_9.UseVisualStyleBackColor = true;
+            this.btn_9.Click += new System.EventHandler(this.btn_9_Click);
+            // 
+            // btn_8
+            // 
+            this.btn_8.FlatAppearance.BorderSize = 0;
+            this.btn_8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_8.Location = new System.Drawing.Point(153, 368);
+            this.btn_8.Name = "btn_8";
+            this.btn_8.Size = new System.Drawing.Size(54, 51);
+            this.btn_8.TabIndex = 7;
+            this.btn_8.Text = "8";
+            this.btn_8.UseVisualStyleBackColor = true;
+            this.btn_8.Click += new System.EventHandler(this.btn_8_Click);
+            // 
+            // btn_7
+            // 
+            this.btn_7.FlatAppearance.BorderSize = 0;
+            this.btn_7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_7.Location = new System.Drawing.Point(78, 368);
+            this.btn_7.Name = "btn_7";
+            this.btn_7.Size = new System.Drawing.Size(54, 51);
+            this.btn_7.TabIndex = 6;
+            this.btn_7.Text = "7";
+            this.btn_7.UseVisualStyleBackColor = true;
+            this.btn_7.Click += new System.EventHandler(this.btn_7_Click);
+            // 
+            // btn_6
+            // 
+            this.btn_6.FlatAppearance.BorderSize = 0;
+            this.btn_6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_6.Location = new System.Drawing.Point(237, 296);
+            this.btn_6.Name = "btn_6";
+            this.btn_6.Size = new System.Drawing.Size(54, 51);
+            this.btn_6.TabIndex = 5;
+            this.btn_6.Text = "6";
+            this.btn_6.UseVisualStyleBackColor = true;
+            this.btn_6.Click += new System.EventHandler(this.btn_6_Click);
+            // 
+            // btn_5
+            // 
+            this.btn_5.FlatAppearance.BorderSize = 0;
+            this.btn_5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_5.Location = new System.Drawing.Point(153, 296);
+            this.btn_5.Name = "btn_5";
+            this.btn_5.Size = new System.Drawing.Size(54, 51);
+            this.btn_5.TabIndex = 4;
+            this.btn_5.Text = "5";
+            this.btn_5.UseVisualStyleBackColor = true;
+            this.btn_5.Click += new System.EventHandler(this.btn_5_Click);
+            // 
+            // btn_4
+            // 
+            this.btn_4.FlatAppearance.BorderSize = 0;
+            this.btn_4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_4.Location = new System.Drawing.Point(78, 296);
+            this.btn_4.Name = "btn_4";
+            this.btn_4.Size = new System.Drawing.Size(54, 51);
+            this.btn_4.TabIndex = 3;
+            this.btn_4.Text = "4";
+            this.btn_4.UseVisualStyleBackColor = true;
+            this.btn_4.Click += new System.EventHandler(this.btn_4_Click);
+            // 
+            // btn_3
+            // 
+            this.btn_3.FlatAppearance.BorderSize = 0;
+            this.btn_3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_3.Location = new System.Drawing.Point(237, 222);
+            this.btn_3.Name = "btn_3";
+            this.btn_3.Size = new System.Drawing.Size(54, 51);
+            this.btn_3.TabIndex = 2;
+            this.btn_3.Text = "3";
+            this.btn_3.UseVisualStyleBackColor = true;
+            this.btn_3.Click += new System.EventHandler(this.btn_3_Click);
+            // 
+            // btn_2
+            // 
+            this.btn_2.FlatAppearance.BorderSize = 0;
+            this.btn_2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_2.Location = new System.Drawing.Point(153, 222);
+            this.btn_2.Name = "btn_2";
+            this.btn_2.Size = new System.Drawing.Size(54, 51);
+            this.btn_2.TabIndex = 1;
+            this.btn_2.Text = "2";
+            this.btn_2.UseVisualStyleBackColor = true;
+            this.btn_2.Click += new System.EventHandler(this.btn_2_Click);
+            // 
+            // btn_1
+            // 
+            this.btn_1.FlatAppearance.BorderSize = 0;
+            this.btn_1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_1.Location = new System.Drawing.Point(78, 222);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(54, 51);
+            this.btn_1.TabIndex = 0;
+            this.btn_1.Text = "1";
+            this.btn_1.UseVisualStyleBackColor = true;
+            this.btn_1.Click += new System.EventHandler(this.btn_1_Click);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.FlatAppearance.BorderSize = 0;
+            this.btn_delete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_delete.Location = new System.Drawing.Point(237, 439);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(54, 23);
+            this.btn_delete.TabIndex = 11;
+            this.btn_delete.Text = "Delete";
+            this.btn_delete.UseVisualStyleBackColor = true;
             // 
             // LoginPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.roundButton2);
-            this.Controls.Add(this.roundButton1);
+            this.ClientSize = new System.Drawing.Size(384, 527);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_0);
+            this.Controls.Add(this.btn_9);
+            this.Controls.Add(this.btn_8);
+            this.Controls.Add(this.btn_7);
+            this.Controls.Add(this.btn_6);
+            this.Controls.Add(this.btn_5);
+            this.Controls.Add(this.btn_4);
+            this.Controls.Add(this.btn_3);
+            this.Controls.Add(this.btn_2);
+            this.Controls.Add(this.btn_1);
             this.Name = "LoginPage";
             this.Text = "LoginPage";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private RoundButton roundButton1;
-        private RoundButton roundButton2;
+        private RoundButton btn_1;
+        private RoundButton btn_2;
+        private RoundButton btn_3;
+        private RoundButton btn_4;
+        private RoundButton btn_5;
+        private RoundButton btn_6;
+        private RoundButton btn_7;
+        private RoundButton btn_8;
+        private RoundButton btn_9;
+        private RoundButton btn_0;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_delete;
     }
 }
--- Project1.4/Main/LoginPage.cs
+++ Project1.4/Main/LoginPage.cs
@@ -12,9 +12,122 @@ namespace ChapeauUI
 {
     public partial class LoginPage : Form
     {
+        private string password;
+        private int count = 0;
+
         public LoginPage()
         {
             InitializeComponent();
+        }
+
+        private void checkPasswod(string input)
+        {
+            if(count == 4)
+            {
+
+            }
+            if(input == "1")
+            {
+                textBox1.AppendText(input);
+                count++;
+            }
+            if(input == "2")
+            {
+                textBox1.AppendText(input);
+                count++;
+            }
+            if(input == "3")
+            {
+                textBox1.AppendText(input);
+                count++;
+            }
+            if (input == "4")
+            {
+                textBox1.AppendText(input);
+                count++;
+            }
+            if (input == "5")
+            {
+                textBox1.AppendText(input);
+                count++;
+            }
+            if (input == "6")
+            {
+                textBox1.AppendText(input);
+                count++;
+            }
+            if (input == "7")
+            {
+                textBox1.AppendText(input);
+                count++;
+            }
+            if (input == "8")
+            {
+                textBox1.AppendText(input);
+                count++;
+            }
+            if (input == "9")
+            {
+                textBox1.AppendText(input);
+                count++;
+            }
+            if (input == "0")
+            {
+                textBox1.AppendText(input);
+                count++;
+                
+            }
+
+        }
+
+        private void btn_1_Click(object sender, EventArgs e)
+        {
+            checkPasswod("1");
+        }
+
+        private void btn_2_Click(object sender, EventArgs e)
+        {
+            checkPasswod("2");
+        }
+
+        private void btn_3_Click(object sender, EventArgs e)
+        {
+            checkPasswod("3");
+        }
+
+        private void btn_4_Click(object sender, EventArgs e)
+        {
+            checkPasswod("4");
+        }
+
+        private void btn_5_Click(object sender, EventArgs e)
+        {
+            checkPasswod("5");
+        }
+
+        private void btn_6_Click(object sender, EventArgs e)
+        {
+            checkPasswod("6");
+        }
+
+        private void btn_7_Click(object sender, EventArgs e)
+        {
+            checkPasswod("7");
+        }
+
+        private void btn_8_Click(object sender, EventArgs e)
+        {
+            checkPasswod("8");
+        }
+
+        private void btn_9_Click(object sender, EventArgs e)
+        {
+            checkPasswod("9");
+        }
+
+        private void btn_0_Click(object sender, EventArgs e)
+        {
+            checkPasswod("0");
         }
     }
 }
