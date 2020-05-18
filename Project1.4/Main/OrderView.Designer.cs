e240a9d596b2e6c567db272f114b7d54af77aa8f
Kegluneg <54842759+Kegluneg@users.noreply.github.com>
2020-05-18 15:14:06 +0200

    Order view
    Added a for for the order view

--- Project1.4/Main/OrderView.Designer.cs
+++ Project1.4/Main/OrderView.Designer.cs
@@ -28,12 +28,147 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.items = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.qty = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lblTableNo = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblWaiter = new System.Windows.Forms.Label();
+            this.lblOrderNo = new System.Windows.Forms.Label();
+            this.btnDone = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.items,
+            this.qty});
+            this.listView1.ForeColor = System.Drawing.Color.White;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(39, 166);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(386, 331);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // items
+            // 
+            this.items.Text = "Order Items";
+            this.items.Width = 267;
+            // 
+            // qty
+            // 
+            this.qty.Text = "Quantity";
+            this.qty.Width = 115;
+            // 
+            // lblTableNo
+            // 
+            this.lblTableNo.AutoSize = true;
+            this.lblTableNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTableNo.ForeColor = System.Drawing.Color.White;
+            this.lblTableNo.Location = new System.Drawing.Point(3, 19);
+            this.lblTableNo.Name = "lblTableNo";
+            this.lblTableNo.Size = new System.Drawing.Size(111, 31);
+            this.lblTableNo.TabIndex = 1;
+            this.lblTableNo.Text = "Table #";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.PaleGreen;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(0, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(465, 52);
+            this.panel1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(184, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 24);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Table Order";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel2.Controls.Add(this.lblOrderNo);
+            this.panel2.Controls.Add(this.lblWaiter);
+            this.panel2.Controls.Add(this.lblTableNo);
+            this.panel2.Location = new System.Drawing.Point(39, 79);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(386, 67);
+            this.panel2.TabIndex = 3;
+            // 
+            // lblWaiter
+            // 
+            this.lblWaiter.AutoSize = true;
+            this.lblWaiter.ForeColor = System.Drawing.Color.White;
+            this.lblWaiter.Location = new System.Drawing.Point(272, 10);
+            this.lblWaiter.Name = "lblWaiter";
+            this.lblWaiter.Size = new System.Drawing.Size(53, 13);
+            this.lblWaiter.TabIndex = 2;
+            this.lblWaiter.Text = "WAITER:";
+            // 
+            // lblOrderNo
+            // 
+            this.lblOrderNo.AutoSize = true;
+            this.lblOrderNo.ForeColor = System.Drawing.Color.White;
+            this.lblOrderNo.Location = new System.Drawing.Point(275, 36);
+            this.lblOrderNo.Name = "lblOrderNo";
+            this.lblOrderNo.Size = new System.Drawing.Size(56, 13);
+            this.lblOrderNo.TabIndex = 3;
+            this.lblOrderNo.Text = "ORDER #";
+            // 
+            // btnDone
+            // 
+            this.btnDone.Location = new System.Drawing.Point(172, 503);
+            this.btnDone.Name = "btnDone";
+            this.btnDone.Size = new System.Drawing.Size(113, 33);
+            this.btnDone.TabIndex = 4;
+            this.btnDone.Text = "DONE";
+            this.btnDone.UseVisualStyleBackColor = true;
+            // 
+            // OrderView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(466, 548);
+            this.Controls.Add(this.btnDone);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listView1);
+            this.Name = "OrderView";
             this.Text = "OrderView";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader items;
+        private System.Windows.Forms.ColumnHeader qty;
+        private System.Windows.Forms.Label lblTableNo;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblOrderNo;
+        private System.Windows.Forms.Label lblWaiter;
+        private System.Windows.Forms.Button btnDone;
     }
 }
