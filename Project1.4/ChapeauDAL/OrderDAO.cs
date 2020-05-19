49cfa4f548115e3f869a9bc37ef4bc45cf820438
Kegluneg <54842759+Kegluneg@users.noreply.github.com>
2020-05-19 13:01:50 +0200

    Create OrderDAO.cs

---
+++ Project1.4/ChapeauDAL/OrderDAO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting;
+using System.Diagnostics;
+
+
+namespace ChapeauDAL
+{
+    class OrderDAO
+    {
+        //...
+    }
+}
