106da4b6db389f707022124c920488209fad01b6
sharelyn_jones <sajones.21@hotmail.com>
2020-05-20 13:27:43 +0200

    Update price calculations

--- Project1.4/ChapeauDAL/MenuItemsDAO.cs
+++ Project1.4/ChapeauDAL/MenuItemsDAO.cs
@@ -8,6 +8,6 @@ namespace ChapeauDAL
 {
     class MenuItemsDAO
     {
-        //...
+        
     }
 }
--- Project1.4/Main/MenuDinnerPage.cs
+++ Project1.4/Main/MenuDinnerPage.cs
@@ -10,8 +10,11 @@ using System.Windows.Forms;
 
 namespace ChapeauUI
 {
+
     public partial class MenuDinnerPage : Form
     {
+        public float totalDinnerPrice = 0;
+
         public MenuDinnerPage()
         {
             InitializeComponent();
@@ -64,8 +67,78 @@ namespace ChapeauUI
 
         private void Tot_Calc_Dinner_Click(object sender, EventArgs e)
         {
-            
+            tot_price_dinner.Text = string.Format("{0:0.00}", Update_Price());
         }
+
+        private float Update_Price()
+        {
+            if (!string.IsNullOrEmpty(txtD1.Text))
+            {
+                totalDinnerPrice += (int.Parse(txtD1.Text) * float.Parse(lblD1price.Text));
+            }
+            if (!string.IsNullOrEmpty(txtD2.Text))
+            {
+                totalDinnerPrice += (int.Parse(txtD2.Text) * float.Parse(lblD2price.Text));
+            }
+            if (!string.IsNullOrEmpty(txtD3.Text))
+            {
+                totalDinnerPrice += (int.Parse(txtD3.Text) * float.Parse(lblD3price.Text));
+            }
+            if (!string.IsNullOrEmpty(txtD4.Text))
+            {
+                totalDinnerPrice += (int.Parse(txtD4.Text) * float.Parse(lblD4price.Text));
+            }
+            if (!string.IsNullOrEmpty(txtD5.Text))
+            {
+                totalDinnerPrice +=  (int.Parse(txtD5.Text) * float.Parse(lblD5price.Text));
+            }
+            if (!string.IsNullOrEmpty(txtD6.Text))
+            {
+                totalDinnerPrice += (int.Parse(txtD6.Text) * float.Parse(lblD6price.Text));
+            }
+            if (!string.IsNullOrEmpty(txtD7.Text))
+            {
+                totalDinnerPrice += (int.Parse(txtD7.Text) * float.Parse(lblD7price.Text));
+            }
+            if (!string.IsNullOrEmpty(txtD8.Text))
+            {
+                totalDinnerPrice += (int.Parse(txtD8.Text) * float.Parse(lblD8price.Text));
+            }
+            if (!string.IsNullOrEmpty(txtD9.Text))
+            {
+                totalDinnerPrice += (int.Parse(txtD9.Text) * float.Parse(lblD9price.Text));
+            }
+            if (!string.IsNullOrEmpty(txtD10.Text))
+            {
+                totalDinnerPrice += (int.Parse(txtD10.Text) * float.Parse(lblD9price.Text));
+            }
+            if (!string.IsNullOrEmpty(txtD11.Text))
+            {
+                totalDinnerPrice += (int.Parse(txtD11.Text) * float.Parse(lblD10price.Text));
+            }
+            if (!string.IsNullOrEmpty(txtD12.Text))
+            {
+                totalDinnerPrice += (int.Parse(txtD12.Text) * float.Parse(lblD12price.Text));
+            }
+            if (!string.IsNullOrEmpty(txtD13.Text))
+            {
+                totalDinnerPrice += (int.Parse(txtD13.Text) * float.Parse(lblD13price.Text));
+            }
+            if (!string.IsNullOrEmpty(txtD14.Text))
+            {
+                totalDinnerPrice += (int.Parse(txtD14.Text) * float.Parse(lblD14price.Text));
+            }
+            if (!string.IsNullOrEmpty(txtD15.Text))
+            {
+                totalDinnerPrice += (int.Parse(txtD15.Text) * float.Parse(lblD15price.Text));
+            }
+            return totalDinnerPrice;
+            
+  
+
+        }
+
+
 
         
     }
