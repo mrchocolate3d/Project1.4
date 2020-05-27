4b47f45883a7fa16a2b4e9395b85638bbede0ee0
Seyifunmi Gandonu <655453@student.inholland.nl>
2020-05-27 15:19:52 +0200

    Update PaymentDAO.cs

--- Project1.4/ChapeauDAL/PaymentDAO.cs
+++ Project1.4/ChapeauDAL/PaymentDAO.cs
@@ -9,12 +9,31 @@ using System.Data;
 
 namespace ChapeauDAL
 {
-    class PaymentDAO: Base
+    public class PaymentDAO : Base
     {
-        //public List<Payment> GetPayments()
-        //{
-        //    string query = "";
-        //    SqlParameter[] sqlParameter = new SqlParameter[0];
-        //}
+        public List<Payment> GetPayments()
+        {
+            string query = "";
+            SqlParameter[] sqlParameters = new SqlParameter[0];
+            return ReadInfoForPayments(ExecuteSelectQuery(query, sqlParameters));
+        }
+        private List<Payment> ReadInfoForPayments(DataTable paymenttable)
+        {
+            List<Payment> payments = new List<Payment>();
+
+            foreach (DataRow dataRow in paymenttable.Rows)
+            {
+                Payment payment = new Payment()
+                {
+                    paymentID = (int)dataRow["paymentID"],
+                    employeeID = (int)dataRow["employeeID"],
+                    dateorder = (DateTime)dataRow["dateorder"],
+                    vat = (double)dataRow["vat"],
+                    TotalPrice = (double)dataRow["totalprice"]
+                };
+                payments.Add(payment);
+            }
+            return payments;
+        }
     }
 }
