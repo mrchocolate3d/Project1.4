a2ab8908df8408a929c0d22ddf910695707020e2
Seyifunmi Gandonu <655453@student.inholland.nl>
2020-06-10 12:56:17 +0200

    Revert "OrdersList interactable"
    This reverts commit 772788f88e95ed488734619077ca0dec938174af.

--- Project1.4/Main/OrdersList.Designer.cs
+++ Project1.4/Main/OrdersList.Designer.cs
@@ -28,8 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnOngoing = new System.Windows.Forms.Button();
-            this.btnComplete = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.listOrders = new System.Windows.Forms.ListView();
             this.tableID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.orderID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
@@ -38,27 +38,27 @@
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // btnOngoing
+            // button1
             // 
-            this.btnOngoing.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.btnOngoing.Location = new System.Drawing.Point(111, 113);
-            this.btnOngoing.Name = "btnOngoing";
-            this.btnOngoing.Size = new System.Drawing.Size(208, 48);
-            this.btnOngoing.TabIndex = 0;
-            this.btnOngoing.Text = "In Progress";
-            this.btnOngoing.UseVisualStyleBackColor = false;
-            this.btnOngoing.Click += new System.EventHandler(this.btnOngoing_Click);
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button1.Location = new System.Drawing.Point(111, 113);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(208, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "In Progress";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // btnComplete
+            // button3
             // 
-            this.btnComplete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.btnComplete.Location = new System.Drawing.Point(348, 113);
-            this.btnComplete.Name = "btnComplete";
-            this.btnComplete.Size = new System.Drawing.Size(208, 48);
-            this.btnComplete.TabIndex = 2;
-            this.btnComplete.Text = "Completed Orders";
-            this.btnComplete.UseVisualStyleBackColor = false;
-            this.btnComplete.Click += new System.EventHandler(this.btnComplete_Click);
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button3.Location = new System.Drawing.Point(348, 113);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(208, 48);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Completed Orders";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // listOrders
             // 
@@ -74,7 +74,6 @@
             this.listOrders.TabIndex = 3;
             this.listOrders.UseCompatibleStateImageBehavior = false;
             this.listOrders.View = System.Windows.Forms.View.Details;
-            this.listOrders.ItemActivate += new System.EventHandler(this.listOrders_ItemActivate);
             // 
             // tableID
             // 
@@ -114,8 +113,8 @@
             this.ClientSize = new System.Drawing.Size(661, 666);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.listOrders);
-            this.Controls.Add(this.btnComplete);
-            this.Controls.Add(this.btnOngoing);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
             this.Name = "OrdersList";
             this.Text = "OrdersList";
             this.panel1.ResumeLayout(false);
@@ -126,8 +125,8 @@
 
         #endregion
 
-        private System.Windows.Forms.Button btnOngoing;
-        private System.Windows.Forms.Button btnComplete;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.ListView listOrders;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
