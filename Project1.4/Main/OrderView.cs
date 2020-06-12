0141e5c79b51dbe82ee66d9c096da6870739b8a9
Seyifunmi Gandonu <655453@student.inholland.nl>
2020-06-12 17:36:37 +0200

    Update OrderView.cs

--- Project1.4/Main/OrderView.cs
+++ Project1.4/Main/OrderView.cs
@@ -17,7 +17,6 @@ namespace ChapeauUI
         Employee employee;
         Order order;
         MenuItemService details = new MenuItemService();
-        private Timer timer1;
 
         public OrderView(Employee employee,Order order)
         {
@@ -27,17 +26,6 @@ namespace ChapeauUI
             ViewOrder();
         }
 
-        public void InitTimer()
-        {
-            timer1 = new Timer();
-            timer1.Tick += new EventHandler(timer1_Tick);
-            timer1.Interval = 10000; // in miliseconds
-            timer1.Start();
-        }
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            
-        }
 
         void ViewOrder()
         {
