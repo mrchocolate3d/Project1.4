8bff31350cafa814671daf2a38b42ed5a64428fe
Kegluneg <54842759+Kegluneg@users.noreply.github.com>
2020-06-27 20:55:24 +0200

    Update OrderMenuItemService.cs

--- Project1.4/ChapeauLogic/OrderMenuItemService.cs
+++ Project1.4/ChapeauLogic/OrderMenuItemService.cs
@@ -31,6 +31,32 @@ namespace ChapeauLogic
                 return null;
             }
         }
+        public List<OrderMenuItem> GetMenuItemsDishes(int orderno)
+        {
+            try
+            {
+                List<OrderMenuItem> order = menuItem_db.db_Get_MenuItems_Dishes(orderno);
+                return order;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+        }
+        public List<OrderMenuItem> GetMenuItemsDrinks(int orderno)
+        {
+            try
+            {
+                List<OrderMenuItem> order = menuItem_db.db_Get_MenuItems_Drinks(orderno);
+                return order;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+        }
         public List<OrderMenuItem> GetAllInfoForPayments(Table table)
         {
             try
