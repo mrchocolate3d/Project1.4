52468b07e25beb099f2d79aa5b3f19f42e22f6b0
Abhi6921 <54932316+Abhi6921@users.noreply.github.com>
2020-06-29 00:53:15 +0200

    Update in payment part of the code.

--- Project1.4/ChapeauDAL/OrderMenuItemDAO.cs
+++ Project1.4/ChapeauDAL/OrderMenuItemDAO.cs
@@ -127,7 +127,7 @@ namespace ChapeauDAL
         // Saves the orders of the payment into the database
         public void SaveOrders(int paymentid, int orderid, int employeeID, string paymentmethod, DateTime dateTime,float vat, float tip, float totalprice)
         {
-            string query = $"INSERT INTO payment(paymentID,orderID,employeeID,paymentMethod,vat,tip,totalprice) VALUES({paymentid++}, {orderid}, {employeeID}, {paymentmethod},{dateTime} ,{vat}, {tip}, {totalprice})";
+            string query = $"INSERT INTO payment(paymentID,orderID,employeeID,paymentMethod,vat,tip,totalprice) VALUES({paymentid}, {orderid}, {employeeID}, {paymentmethod},{dateTime} ,{vat}, {tip}, {totalprice})";
             SqlParameter[] sqlParameter = new SqlParameter[0];
             ExecuteEditQuery(query, sqlParameter);
         }
